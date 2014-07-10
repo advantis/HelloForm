93402127c765840ee7e5afb0443979891d46ee4b
Yuri Kotov <yuri.kotov@yalantis.com>
2014-07-10 20:26:10 +0300

    Removed placeholder event initialization

--- HelloForm/AppDelegate.swift
+++ HelloForm/AppDelegate.swift
@@ -8,17 +8,5 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow!
-
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
-        let navigationController = window.rootViewController as UINavigationController
-        let viewController = navigationController.topViewController as ViewController
-
-        let event = Event(id:1)
-        event.quantity = 4
-
-        viewController.setEvent(event)
-
-        return true
-    }
 }
 
