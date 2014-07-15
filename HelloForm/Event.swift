f3a9a4743648ae78f8dab81ecf0983940a4b33f4
Yuri Kotov <yuri.kotov@yalantis.com>
2014-07-15 14:21:54 +0300

    Changed Event designated initialiser

--- HelloForm/Event.swift
+++ HelloForm/Event.swift
@@ -2,13 +2,22 @@
 //  Copyright © 2014 Yuri Kotov
 //
 
+import Foundation
+
 @objc
 class Event {
     
     let id: Int
-    var quantity = 0
+    let name: String
+    let date: NSDate
+    let price: Double
+    let quantity: Int
 
-    init(id: Int) {
-        self.id = id
+    init(attributes: JSON, context: EventContext) {
+        id = attributes["id"]!
+        name = attributes["name"]!
+        price = attributes["price"]!
+        quantity = attributes["quantity"]!
+        date = context.dateFormatter.dateFromString(attributes["date"]!)
     }
 }
