fc392db9aa9bf1ac08c2802b33a45210fd720227
Yuri Kotov <yuri.kotov@yalantis.com>
2014-07-15 15:13:42 +0300

    Improved EventListViewController class structure

--- HelloForm/EventListViewController.swift
+++ HelloForm/EventListViewController.swift
@@ -7,29 +7,17 @@ import UIKit
 class EventListViewController: UITableViewController, EventContext {
 
     var events: Event[] = []
-    @lazy var dateFormatter = NSDateFormatter(format: "dd/MM/yy")
 
-    func prepareForPurchase(destination: AnyObject!) {
-        let indexPath = tableView.indexPathForSelectedRow()
-        let presenter = destination as ViewController
-        presenter.setEvent(events[indexPath.row], withContext:self)
-    }
+    @lazy
+    var dateFormatter = NSDateFormatter(format: "dd/MM/yy")
 
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        switch segue.identifier! {
-            case "buy":
-                prepareForPurchase(segue.destinationViewController)
-            default:
-                super.prepareForSegue(segue, sender: sender)
-        }
-    }
-
-    func loadEvents() {
-        Event.listEvents({result in
+    func reloadData() {
+        // TODO: Replace 'weak' with 'unowned' when it stop crashing
+        Event.listEvents({[weak self] result in
             switch result {
                 case .Success(let events):
-                    self.events = events
-                    self.tableView.reloadData()
+                    self!.events = events
+                    self!.tableView.reloadData()
                 case .Failure(let error):
                     println(error)
             }
@@ -38,16 +26,32 @@ class EventListViewController: UITableViewController, EventContext {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadEvents()
+        reloadData()
     }
 
     override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
         return events.count
     }
 
+    // TODO: Extract into generic data source
     override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
         let cell = tableView.dequeueReusableCellWithIdentifier("EventListCell", forIndexPath: indexPath) as EventListCell
         cell.setEvent(events[indexPath.row], withContext:self)
         return cell
+    }
+
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        switch segue.identifier! {
+        case "buy":
+            prepareForPurchase(segue.destinationViewController)
+        default:
+            super.prepareForSegue(segue, sender: sender)
+        }
+    }
+
+    func prepareForPurchase(destination: AnyObject!) {
+        let indexPath = tableView.indexPathForSelectedRow()
+        let presenter = destination as EventPresenter
+        presenter.setEvent(events[indexPath.row], withContext:self)
     }
 }
